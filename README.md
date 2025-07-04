99c6135c2355bcd8e9b7fbae80315c556af3da80
wangrongfa <wangrongfa@hangzhouhaobo.com>
2025-07-04 10:44:18 +0800

    init

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # SkyeIOT
 各种物联网行业demo，比如ocpp2.0, mqtt、modbus协议
+
+Apollo配置中心
---
+++ ocpp-demo/src/main/java/com/example/ocppdemo/OcppDemoApplication.java
@@ -0,0 +1,16 @@
+package com.example.ocppdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/*
+https://gitcode.com/gh_mirrors/ja/Java-OCA-OCPP/blob/master/ocpp-v2_0/pom.xml
+ */
+@SpringBootApplication
+public class OcppDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OcppDemoApplication.class, args);
+    }
+
+}
